960907693b8b4e992462be834d12feded4d53031
David <lin.david985@gmail.com>
2020-02-04 20:29:53 -0500

    updated secrets folders

---
+++ Secrets/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  WeatherApp
+//
+//  Created by David Lin on 2/4/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let pixaBayKey = "14969843-2cae0700ee723f2830a9ec598"
+    static let darkSkyKey = "0f07fd66b57aa09ff71fd0899d66ecdf"
+}
